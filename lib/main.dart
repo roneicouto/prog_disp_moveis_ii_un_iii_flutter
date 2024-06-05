d3912856839945baac8e146d3a64ad54e692f165
Ronei Couto Melo <registros@roneicouto.com>
2024-06-05 13:34:48 +0000

    teste configuração idx

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //title: 'Programação Dispositivos Móveis II - 2024',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
